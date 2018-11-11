3f2eddd24dfd728be1602e721a46879750ecd507
Sharma-Hrishabh <30hrs1999@gmail.com>
2018-11-11 13:18:11 +0530

    doctors model modified

--- hospital_management/models.py
+++ hospital_management/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Doctor(models.Model):
     doctor_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    charge = models.TextField()
+    charge = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
